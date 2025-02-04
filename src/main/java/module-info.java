d7ea89f8f43ef6a9837a253f68145ec374177591
Adrian Saavedra <153601542+cronoxxxx@users.noreply.github.com>
2025-02-04 09:08:47 +0100

    marcos error 4 febrero

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,6 @@ module org.example.pesadillamago {
     exports org.example.pesadillamago;
     opens org.example.pesadillamago to javafx.fxml;
     opens org.example.pesadillamago.game.dungeon.home to javafx.fxml;
+    exports org.example.pesadillamago.ui.marcos;
+    opens org.example.pesadillamago.ui.marcos to javafx.fxml;
 }
---
+++ src/main/java/org/example/pesadillamago/ui/marcos/HelloApplication.java
@@ -0,0 +1,23 @@
+package org.example.pesadillamago.ui.marcos;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/pesadillamago/mainHomeInterface.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("Main Home Interface");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
